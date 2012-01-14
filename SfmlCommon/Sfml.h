c72c6bb70b36a0fbd290569fa4976aa56f9823c3
bastienvalentin <bastienvalentin@gmail.com>
2012-01-14 15:51:46 +0000

    caca

--- SfmlCommon/Sfml.h
+++ SfmlCommon/Sfml.h
@@ -1,15 +1,10 @@
 #ifndef SFML_H
 #define SFML_H
 
-#ifdef OS_WINDOWS
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#else
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include "SFML/Audio.hpp"
 #include "SFML/Network.hpp"
 #include "SFML/System.hpp"
 
-#endif
 #endif // SFML_H
